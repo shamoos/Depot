e505895cf9aa48f647083266dfac92a9f9780094
shamoos <shamoos@gmail.com>
2014-01-16 16:44:42 +0400

    Product title length checking

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: { 
     with: %r{\.(gif|jpg|png)\z}i, 
     message: 'URL должен указывать на изображение в формате GIF, JPG или PNG!'
