ee33641ae5ab7f84d5a8ba0c0752f8cc0768131e
shamoos <shamoos@gmail.com>
2014-01-16 16:21:56 +0400

    Adding unit&func tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+      @update = {
+	title: 'Shamoos',
+	description: 'Big Shamoos Great Ruby Developer',
+	image_url: 'shamoos.jpg',
+	price: 100.99
+      }
   end
 
   test "should get index" do
@@ -18,7 +24,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
+#{ description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +42,8 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    patch :update, id: @product.to_param, product: @update
+#{ description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     assert_redirected_to product_path(assigns(:product))
   end
 
